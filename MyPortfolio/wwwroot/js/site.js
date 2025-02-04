4e0bb5d74e12a928172f3143a72fa2873d695fa2
Karina Kristensen <karinakristensen2@gmail.com>
2025-02-04 10:16:46 +0100

    Changes in "about me"

--- MyPortfolio/wwwroot/js/site.js
+++ MyPortfolio/wwwroot/js/site.js
@@ -24,3 +24,12 @@ function toggleSection(id) {
     }
 }
 
+function toggleSection(id) {
+    var content = document.getElementById(id);
+    if (content.style.display === "none" || content.style.display === "") {
+        content.style.display = "block";
+    } else {
+        content.style.display = "none";
+    }
+}
+
